f1257badc20de175c00c4d0aaf39999a1459a670
Martin Fernandez Funes <mfernandezfunes@gmail.com>
2025-11-16 00:35:43 -0300

    Initial Commit

---
+++ musica-player/musica_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  musica_playerApp.swift
+//  musica-player
+//
+//  Created by Martin Fernandez on 16/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct musica_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musica-playerTests/musica_playerTests.swift
@@ -0,0 +1,17 @@
+//
+//  musica_playerTests.swift
+//  musica-playerTests
+//
+//  Created by Martin Fernandez on 16/11/2025.
+//
+
+import Testing
+@testable import musica_player
+
+struct musica_playerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
